3c6610e0fde2b82d5cbae8c91b6fdb2bca368885
YanaLugina <yana4961@gmail.com>
2019-08-09 01:17:55 +1000

    unmount: componentDidiCatch: created new component button for calling error

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -5,12 +5,15 @@ import Header from '../header';
 import RandomPlanet from '../random-planet';
 import ItemList from '../item-list';
 import PersonDetails from '../person-details';
+import ErrorButton from "../error-button";
+import ErrorIndicator from "../error-indicator";
 
 import './app.css';
 
 export default class App extends Component {
     state = {
-        selectedPerson: 5
+        selectedPerson: 5,
+        hasError: false
     };
 
     onPersonSelected = (id) => {
@@ -19,11 +22,23 @@ export default class App extends Component {
         });
     };
 
+    componentDidCatch() {
+        console.log('componentDidCatch');
+        this.setState({ hasError: true });
+    }
+
     render() {
+
+        if(this.state.hasError) {
+            return <ErrorIndicator />
+        }
+
         return (
             <div  className="stardb-app">
                 <Header />
                 <RandomPlanet />
+
+                <ErrorButton />
 
                 <div className="row mb2">
                     <div className="col-md-6">
